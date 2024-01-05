de8e7504d984c09df34fb4e6b17cd2ad175201c5
vflei <vflei@Vladimir.attlocal.net>
2024-01-04 20:21:22 -0500

    Initial Commit

---
+++ src/ProjectE/DemoE.java
@@ -0,0 +1,14 @@
+package ProjectE;
+
+public class DemoE {
+	
+public static void main(String[]args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Git project push...");
+		System.out.println("Git project pull...");
+		
+	}
+
+}
